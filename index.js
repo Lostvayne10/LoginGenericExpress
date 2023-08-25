c583ea4c740cec0baa8cf783062d1726d6a65e1c
Alejandro Infante <antonio.infante@alumnos.udg.mx>
2023-08-24 21:54:41 -0600

    Correciones para tener envio de correos con nodemailer

--- index.js
+++ index.js
@@ -2,12 +2,32 @@ import express from "express";
 import dotenv from "dotenv";
 import conectarDB from "./config/db.js";
 import routerUsuarios from "./routes/usuarioRoutes.js";
-
+import cors from "cors"
 
 const app = express();
 app.use(express.json());
 dotenv.config();
 conectarDB();
+
+// CORS
+const whitelist = [
+    process.env.FRONTEND_URL
+];
+
+const corsOptions = {
+    origin: function(origin, callback){
+        if(whitelist.includes(origin)){
+            callback(null, true);
+        }else{
+            callback(new Error("Error de cors"));
+        }
+    }
+} ;
+
+app.use(cors(corsOptions));
+
+
+console.log(Math.random().toString(32).substring(2)+Math.random().toString(32).substring(2)+Math.random().toString(32).substring(2));
 
 // ROUTING
 app.use("/api/usuarios", routerUsuarios);
